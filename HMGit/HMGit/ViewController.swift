10361cb2bfcbf7e94da8b375652c623e2ca7830e
hongmei <350666080@qq.com>
2017-06-23 22:20:24 +0800

    1

---
+++ HMGit/HMGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HMGit
+//
+//  Created by 红梅 on 2017/6/23.
+//  Copyright © 2017年 红梅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
